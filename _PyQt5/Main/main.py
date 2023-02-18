653bcf44004f402e6c7d6e52fa26968248b36ae1
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-02-18 04:57:30 +0300

    .

--- _PyQt5/Main/main.py
+++ _PyQt5/Main/main.py
@@ -1,18 +1,24 @@
 import sys
-from home import *
+from home import Ui_MainWindow
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QDesktopWidget
+from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self, parent=None):
-        QtWidgets.QMainWindow.__init__(self)
+        super(MainWindow, self).__init__()
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.show()
+        self.ui.profileCont.hide() # profileCont widgeti için butonun checked özelliği açık. başlangıçta hide olmalı
+        
+        
+  
     
 
 
 if __name__ == "__main__":
-     app = QtWidgets.QApplication(sys.argv)
+     app = QApplication(sys.argv)
      window=MainWindow()
      window.show()
      sys.exit(app.exec_())
