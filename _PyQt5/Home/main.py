a03497d790b0be4d8239afe631c74fd2f5643cf0
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-02-19 16:33:41 +0300

    .

--- _PyQt5/Home/main.py
+++ _PyQt5/Home/main.py
@@ -31,10 +31,10 @@ class MainWindow(QMainWindow):
     
     
 
-
 if __name__ == "__main__":
      app = QApplication(sys.argv)
      window=MainWindow()
      window.show()
      sys.exit(app.exec_())
+
 
